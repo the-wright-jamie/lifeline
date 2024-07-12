d5412049ddfae84bed160814875298371d910db9
Jamie Wright <jwright3478@gmail.com>
2024-07-12 15:11:57 +0100

    basic work

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,20 +1,12 @@
-import Image from "next/image";
+import MegaHeader from "./mega-header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <h1 className="mb-3 text-5xl font-semibold">Lifeline</h1>
-      </div>
-
-      <div className="mb-32 grid lg:grid-cols-1 lg:text-left">
-          <h2 className="mb-3 text-2xl font-semibold">
-            Loading...
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-      </div>
+      <MegaHeader />
+        <h2 className="mb-3 text-2xl font-semibold">
+          Loading...
+        </h2>
     </main>
   );
 }
