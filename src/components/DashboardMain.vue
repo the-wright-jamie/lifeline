da1b5abea4180b36c37e4c5ad274248c45c1f5ce
Jamie Wright <jwright3478@gmail.com>
2025-02-03 11:26:12 +0000

    More work on sliders

--- src/components/DashboardMain.vue
+++ src/components/DashboardMain.vue
@@ -42,6 +42,21 @@ function updateOffset(input: number) {
   ganttChartLiveUpdate()
 }
 
+function setFocusedDependency(dependency: string) {
+  focusedDependency.value = dependency
+  ganttChartLiveUpdate()
+}
+
+function resetSliders() {
+  let config: Config = JSON.parse(localStorage.getItem('config') || '')
+  config.dashboardConfig.ganttWidth = config.dashboardConfig.ganttMaxWidth / 2
+  userChartWidth.value = config.dashboardConfig.ganttMaxWidth / 2
+  userChartOffset.value = 0
+  focusedDependency.value = 'all'
+  ganttChartLiveUpdate()
+  localStorage.setItem('config', JSON.stringify(config))
+}
+
 let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
 let diagram = ref(``)
 
@@ -86,11 +101,6 @@ allData.forEach((data) => {
   iter++
 })
 
-function setFocusedDependency(dependency: string) {
-  focusedDependency.value = dependency
-  ganttChartLiveUpdate()
-}
-
 function ganttChartLiveUpdate() {
   diagram.value = ``
   if (isDarkMode) {
@@ -119,7 +129,7 @@ function ganttChartLiveUpdate() {
   let unixSeekforwad = unixCurrentTime + unixChartWidth
 
   for (var data in depJson) {
-    if (focusedDependency.value != "all" && data != focusedDependency.value) {
+    if (focusedDependency.value != 'all' && data != focusedDependency.value) {
       continue
     }
 
@@ -206,55 +216,64 @@ let depJsonstring = JSON.stringify(depJson)
   <div v-if="showGantt">
     <h2>Gantt Chart</h2>
     <h2>Chart controls <span class="beta-pill">BETA</span></h2>
-    <Menu as="div" class="relative inline-block text-left">
-      <div>
-        <MenuButton
-          class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white dark:bg-gray-900 px-3 py-2 text-sm font-semibold text-gray-900 dark:text-white ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 dark:hover:bg-gray-600"
-        >
-          Showing: {{ dependencyTitleCase(focusedDependency) }}
-        </MenuButton>
-      </div>
+    <div class="grid auto-cols-2 grid-flow-col gap-4">
+      <Menu as="div" class="relative inline-block text-left">
+        <div>
+          <MenuButton
+            class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white dark:bg-gray-900 px-3 py-2 text-sm font-semibold text-gray-900 dark:text-white ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 dark:hover:bg-gray-600"
+          >
+            Showing: {{ dependencyTitleCase(focusedDependency) }}
+          </MenuButton>
+        </div>
 
-      <transition
-        enter-active-class="transition ease-out duration-100"
-        enter-from-class="transform opacity-0 scale-95"
-        enter-to-class="transform opacity-100 scale-100"
-        leave-active-class="transition ease-in duration-75"
-        leave-from-class="transform opacity-100 scale-100"
-        leave-to-class="transform opacity-0 scale-95"
-      >
-        <MenuItems
-          class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-900 ring-1 shadow-lg ring-black/5 focus:outline-hidden"
+        <transition
+          enter-active-class="transition ease-out duration-100"
+          enter-from-class="transform opacity-0 scale-95"
+          enter-to-class="transform opacity-100 scale-100"
+          leave-active-class="transition ease-in duration-75"
+          leave-from-class="transform opacity-100 scale-100"
+          leave-to-class="transform opacity-0 scale-95"
         >
-          <div class="py-1">
-            <MenuItem>
-              <button
-                @click="setFocusedDependency('all')"
-                class="block px-4 py-2 text-sm not-hyperlink"
-              >
-                Show All
-                <span class="material-symbols-rounded">{{
-                  focusedDependency == 'all' ? '&#xe5ca;' : ''
-                }}</span>
-              </button>
-            </MenuItem>
-            <hr />
-            <MenuItem v-for="dependency in dependencies">
-              <button
-                @click="setFocusedDependency(dependency)"
-                href="#"
-                class="block px-4 py-2 text-sm not-hyperlink"
-              >
-                {{ dependencyTitleCase(dependency) }}
-                <span class="material-symbols-rounded">{{
-                  focusedDependency == dependency ? '&#xe5ca;' : ''
-                }}</span>
-              </button>
-            </MenuItem>
-          </div>
-        </MenuItems>
-      </transition>
-    </Menu>
+          <MenuItems
+            class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-900 ring-1 shadow-lg ring-black/5 focus:outline-hidden"
+          >
+            <div class="py-1">
+              <MenuItem>
+                <button
+                  @click="setFocusedDependency('all')"
+                  class="block px-4 py-2 text-sm not-hyperlink"
+                >
+                  Show All
+                  <span class="material-symbols-rounded">{{
+                    focusedDependency == 'all' ? '&#xe5ca;' : ''
+                  }}</span>
+                </button>
+              </MenuItem>
+              <hr />
+              <MenuItem v-for="dependency in dependencies">
+                <button
+                  @click="setFocusedDependency(dependency)"
+                  href="#"
+                  class="block px-4 py-2 text-sm not-hyperlink"
+                >
+                  {{ dependencyTitleCase(dependency) }}
+                  <span class="material-symbols-rounded">{{
+                    focusedDependency == dependency ? '&#xe5ca;' : ''
+                  }}</span>
+                </button>
+              </MenuItem>
+            </div>
+          </MenuItems>
+        </transition>
+      </Menu>
+      <button
+        @click="resetSliders()"
+        class="relative inline-block text-center w-full justify-center gap-x-1.5 rounded-md bg-white dark:bg-gray-900 px-3 py-2 text-sm font-semibold text-gray-900 dark:text-white ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 dark:hover:bg-gray-600"
+      >
+        Reset Chart
+      </button>
+    </div>
+
     <div class="stickyHeader bg-white dark:bg-black">
       <br />
       <div class="grid auto-cols-2 grid-flow-col gap-4">
@@ -278,7 +297,9 @@ let depJsonstring = JSON.stringify(depJson)
           }}</span>
         </div>
         <div class="relative mb-6">
-          <label for="labels-range-input">Chart Offset: {{ -userChartOffset }} {{ userChartOffset == 1 ? "day" : "days"}}</label>
+          <label for="labels-range-input"
+            >Chart Offset: {{ -userChartOffset }} {{ userChartOffset == 1 ? 'day' : 'days' }}</label
+          >
           <input
             id="labels-range-input"
             type="range"
