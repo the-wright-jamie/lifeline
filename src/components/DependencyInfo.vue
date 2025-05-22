8c4694d7068bc716f4d235d151bff923c61639b5
Jamie Wright <jwright3478@gmail.com>
2025-05-22 15:19:18 +0100

    add dependency view (new feature)

--- src/components/DependencyInfo.vue
+++ src/components/DependencyInfo.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { dependencyTitleCase, getData } from '@/assets/ts/utils'
+import { getFriendlyName, getData, isDateBeforeToday } from '@/assets/ts/utils'
+import { callWithErrorHandling, ref } from 'vue'
+import ErrorMessage from './ErrorMessage.vue'
 const props = defineProps({
   dependency: String
 })
@@ -9,30 +11,196 @@ function toLocalDate(date: string) {
   return date
 }
 
-let dependency_info = await getData(`https://endoflife.date/api/${props.dependency}.json`)
-let eolLink = `https://endoflife.date/${props.dependency}`
+function checkIfDevice() {
+  return dependencyType === 'device'
+}
+
+let error = ref(false)
+let dependency_info = undefined
+
+try {
+  dependency_info = await getData(`https://endoflife.date/api/v1/products/${props.dependency}`)
+  error.value = false
+} catch {
+  error.value = true
+}
+
+let isDarkMode = ''
+let friendlyName = ''
+let iconLink = ''
+let eolLink = ''
+let releasePolicy = ''
+let latestRelease = ''
+let releaseDate = ''
+let latestVersion = ''
+let latestPatchRelease = ''
+let dependencyType = ''
+let howToGetCurrentVersion = ''
+let anyKnownEOES = false
+let anyKnownPatches = false
+
+let depJson = []
+
+if (!error.value) {
+  isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+  friendlyName = dependency_info.result.label
+  iconLink = dependency_info.result.links.icon
+  eolLink = dependency_info.result.links.html
+  releasePolicy = dependency_info.result.links.releasePolicy
+  latestRelease = dependency_info.result.releases[0].label
+  releaseDate = dependency_info.result.releases[0].releaseDate
+  if (dependency_info.result.releases[0].latest != null) {
+    latestVersion = dependency_info.result.releases[0].latest.name
+    latestPatchRelease = dependency_info.result.releases[0].latest.date
+  }
+  dependencyType = dependency_info.result.category
+  howToGetCurrentVersion = dependency_info.result.versionCommand
+  anyKnownEOES = false
+  anyKnownPatches = false
+
+  depJson = []
+  dependency_info.result.releases.forEach((data) => {
+    depJson.push(data)
+  })
+
+  for (var i in depJson) {
+    if (anyKnownEOES != true) {
+      try {
+        anyKnownEOES = depJson[i].eoesFrom != null
+      } catch {
+        console.log('lmao')
+      }
+    }
+
+    if (anyKnownPatches != true) {
+      try {
+        anyKnownPatches = depJson[i].latest != null
+      } catch {
+        console.log('lmao')
+      }
+    }
+  }
+
+  depJson.shift()
+}
+console.log()
 </script>
 
 <template>
-  <h1>
-    <a :href="eolLink" class="not-hyperlink">{{ dependencyTitleCase(dependency) }}</a>
-  </h1>
-  <table class="table-auto">
-    <thead>
-      <tr>
-        <th>Latest Release</th>
-        <th>Release Date</th>
-        <th v-if="dependency_info[0].eol">End-of-life</th>
-        <th v-if="dependency_info[0].latest">Latest</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{{ dependency_info[0].cycle }}</td>
-        <td>{{ toLocalDate(dependency_info[0].releaseDate) }}</td>
-        <td v-if="dependency_info[0].eol">{{ toLocalDate(dependency_info[0].eol) }}</td>
-        <td v-if="dependency_info[0].latest">v{{ dependency_info[0].latest }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div v-if="error">
+    <ErrorMessage
+      header="Dependency not found!"
+      message="We weren't able to find that dependency in the archives. Please check your spelling and try again."
+    />
+  </div>
+  <div v-else>
+    <br />
+    <h1 v-if="iconLink" class="center-with-icons">
+      <img
+        :src="iconLink"
+        class="dependency-icon material-symbols-rounded"
+        :class="isDarkMode ? 'invert' : ''"
+      />
+      {{ friendlyName }}
+    </h1>
+    <h1 v-else class="center">
+      {{ friendlyName }}
+    </h1>
+    <br />
+    <hr />
+    <br />
+    <div class="center">
+      <p>
+        <a :href="eolLink">See on <b>end-of-life.date</b></a> |
+        <a :href="releasePolicy">Release Policy</a>
+      </p>
+    </div>
+    <br />
+    <div class="center">
+      <h2 v-if="checkIfDevice()">The latest device release is the</h2>
+      <h2 v-else>The latest major release is</h2>
+      <h1>{{ latestRelease }}</h1>
+      <h3>released on {{ toLocalDate(releaseDate) }}</h3>
+      <h5 v-if="anyKnownPatches">
+        Current Patch: {{ latestVersion
+        }}<span v-if="latestPatchRelease != null">
+          released on {{ toLocalDate(latestPatchRelease) }}</span
+        >
+      </h5>
+    </div>
+    <div v-if="howToGetCurrentVersion != null">
+      <br />
+      <div class="grid grid-flow-col info-box">
+        <div>
+          <span class="material-symbols-rounded">&#xe88e;</span>
+        </div>
+        <div>
+          <p>Command to get current version:</p>
+          <p>{{ howToGetCurrentVersion }}</p>
+        </div>
+      </div>
+    </div>
+    <br />
+    <hr />
+    <br />
+    <div>
+      <h2>Other Releases</h2>
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">Release</th>
+            <th scope="col" class="px-6 py-3">Release Date</th>
+            <th scope="col" class="px-6 py-3">End of Life</th>
+            <th scope="col" class="px-6 py-3" v-if="anyKnownEOES">End of Support</th>
+            <th scope="col" class="px-6 py-3" v-if="anyKnownPatches">Latest Patch</th>
+          </tr>
+        </thead>
+        <tbody v-for="release in depJson">
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              {{ release.label }}
+            </th>
+            <td class="px-6 py-2">
+              {{ toLocalDate(release.releaseDate) }}
+            </td>
+            <td
+              class="px-6 py-2"
+              :class="
+                isDateBeforeToday(release.eolFrom)
+                  ? release.eolFrom != null
+                    ? 'text-red-600'
+                    : ''
+                  : 'text-green-500'
+              "
+            >
+              {{ release.eolFrom != null ? toLocalDate(release.eolFrom) : 'Unknown' }}
+            </td>
+            <td class="px-6 py-2" v-if="anyKnownEOES">
+              <span
+                :class="
+                  isDateBeforeToday(release.eoesFrom)
+                    ? release.eoesFrom != null
+                      ? 'text-red-600'
+                      : ''
+                    : 'text-green-500'
+                "
+                >{{ release.eoesFrom != null ? toLocalDate(release.eoesFrom) : 'Unknown' }}</span
+              >
+            </td>
+            <td class="px-6 py-2" v-if="anyKnownPatches">
+              {{ release.latest.name
+              }}<span v-if="release.latest != null && release.latest.date"
+                ><br />({{ toLocalDate(release.latest.date) }})</span
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
--- src/views/DependencyView.vue
+++ src/views/DependencyView.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-import DashboardMain from '@/components/DashboardMain.vue'
+import DependencyInfo from '@/components/DependencyInfo.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const loading_info = `Loading info on '${route.params.dependency}''`
 </script>
 
 <template>
+  <Suspense>
+    <DependencyInfo :dependency="route.params.dependency" />
+    <template #fallback> <AnimeSpinner class="middle" :msg="loading_info" /></template>
+  </Suspense>
   <!-- hey, what are you doing here? this is a secret new feature - it's not ready yet! -->
 </template>
